2c82e84bb2ce74cfe9ab491a58c2cbd23fe97e07
Adarsh <38885821+adarshapakala@users.noreply.github.com>
2020-07-28 17:20:54 +0200

    Rgister user and login working
    Rgister user and login working

--- userAccount/views.py
+++ userAccount/views.py
@@ -1,7 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method =='POST':
+        print('login called') 
+        user_name=request.POST['user_name']
+        password=request.POST['password']
+        user=auth.authenticate(username=user_name, password=password)
+        if user is not None:
+            print('login3 called') 
+            auth.login(request,user)
+            messages.info(request, 'Login success')
+            return redirect("/")
+        else:
+            print('login4 called') 
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        list(messages.get_messages(request)) ##Clear the messges while loading the page first time
+        print('login2 called') 
+        return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method =='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        user_name=request.POST['user_name']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        if password1==password2 :
+            if User.objects.filter(username=user_name).exists():
+                messages.info(request, "The Username is already exists")
+                return redirect('register')
+
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "The email address is already exists")
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=user_name, password=password1, email= email, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('login')
+                print('user created')              
+        else:
+            messages.info(request, "The passwords not matching")
+            return redirect('register')
+
+    else:
+        list(messages.get_messages(request)) ##Clear the messges while loading the page first time
+        return render(request, 'register.html')
